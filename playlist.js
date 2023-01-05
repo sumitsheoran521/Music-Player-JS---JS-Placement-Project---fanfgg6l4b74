9b859ea6a8b3bdd4839002be4b1a5015e8d43378
sumit sheoran <sumitsheoran521@gmail.com>
2023-01-06 02:06:28 +0530

    Final Version Jan 2023

--- playlist.js
+++ playlist.js
@@ -14,6 +14,7 @@ const forwardBtn = document.getElementById("forward");
 const backwardBtn = document.getElementById("backward");
 const searchSongs = document.getElementById("searchsong");
 let myProgressBar = document.getElementById("progress-bar");
+
 let previewData;
 let music_list = [];
 let queue_music = [];
@@ -21,20 +22,17 @@ let isRandom = false;
 let isplaying = false;
 let updateTimer;
 let track_index = 0;
+if (localStorage) {
+  let data = JSON.parse(localStorage.getItem("playlist"));
+  if (data) {
+    music_list = data;
+  }
+}
 
-
-//fetching API
 const apiFetch = async () => {
-  const result = await fetch(
-    "https://api.napster.com/v2.1/tracks/top?apikey=YTM3ODVhZDYtNTJhMC00ODUzLThjZjItOGZmMzJjMDliNmQ3"
-  );
-  const data = await result.json();
   const list = document.getElementById("music_list");
   const an = document.getElementById("structure");
-  // Putting data in music-list if not there
-  music_list = data?.tracks;
-  console.log(music_list);
-  data?.tracks?.forEach((item, index) => {
+  music_list?.forEach((item, index) => {
     const child = document.createElement("div");
     child.className = "musicListDiv";
     const img = document.createElement("img");
@@ -49,40 +47,28 @@ const apiFetch = async () => {
       track_index = index;
       now_playing.innerText = item.name;
       audioPlay(item.previewURL);
-    }; 
-
-    // Injecting data to HTML by DOM
+    };
     an.appendChild(child);
     list.appendChild(child);
   });
   previewData = data.tracks[0].previewURL;
-  console.log(previewData);
 };
 
-
-//Audio play
 const audioPlay = async (url) => {
-  // Clearing Interval everytime
   clearInterval(updateTimer);
-  // Calling reset function to clear the time when next song play
   reset();
   player.src = url;
   player.play();
   playbtn.className = "fa-solid fa-4x fa-circle-pause";
-  // Update song time in every 1 sec
   updateTimer = setInterval(setUpdate, 1000);
   isplaying = true;
-  // calling nextTrack function to play next song when previous was over
   player.addEventListener("ended", nextTrack);
 };
-
-// resset function to reset the time
 function reset() {
   curr_time.textContent = "00:00";
   total_duration.textContent = "00:00";
+  // seek_slider.value = 0;
 }
-
-// Applying play and pause feature
 const playandpause = () => {
   const playbtn = document.getElementById("masterPlay");
   playbtn.className = !isplaying
@@ -96,14 +82,12 @@ const playandpause = () => {
 };
 apiFetch();
 
-
-console.log(previewData);
-
-
-// Updating time and coverting it into sec
 function setUpdate() {
-  // if time showing NaN in timer
+  let seekPosition = 0;
   if (!isNaN(player.duration)) {
+    // seekPosition = player.currentTime * (100 / player.duration);
+    // seek_slider.value = seekPosition;
+
     let currentMinutes = Math.floor(player.currentTime / 60);
     let currentSeconds = Math.floor(player.currentTime - currentMinutes * 60);
     let durationMinutes = Math.floor(player.duration / 60);
@@ -127,58 +111,9 @@ function setUpdate() {
   }
 }
 
-// forward 5 sec
 forwardBtn.addEventListener("click", () => {
   player.currentTime += 5;
 });
-
-// backward 5 sec
-backwardBtn.addEventListener("click", () => {
-  player.currentTime -= 5;
-});
-
-// for next track
-function nextTrack() {
-  if (track_index < music_list.length - 1 && isRandom === false) {
-    track_index += 1;
-  } else if (track_index < music_list.length - 1 && isRandom === true) {
-    let random_index = Number.parseInt(Math.random() * music_list.length);
-    track_index = random_index;
-  } else {
-    track_index = 0;
-  }
-  // loadTrack(track_index);
-  now_playing.innerText = music_list[track_index].name;
-  audioPlay(music_list[track_index].previewURL);
-}
-
-// for previous track
-function prevTrack() {
-  if (track_index > 0) {
-    track_index -= 1;
-  } else {
-    track_index = music_list.length - 1;
-  }
-  now_playing.innerText = music_list[track_index].name;
-  audioPlay(music_list[track_index].previewURL);
-}
-next.onclick = nextTrack;
-previous.onclick = prevTrack;
-
-// for shuffling the songs
-shuffle.onclick = () => {
-  isRandom = !isRandom;
-  shuffle.className = isRandom
-  // setting shuffleon class to apply CSS
-    ? "fa-solid fa-1x fa-shuffle shuffleon"
-    : "fa-solid fa-1x fa-shuffle";
-  let random_index = Number.parseInt(Math.random() * music_list.length);
-
-  if (isRandom) {
-    now_playing.innerText = music_list[random_index].name;
-    audioPlay(music_list[random_index].previewURL);
-  }
-};
 
 // Update Seekbar
 player.addEventListener("timeupdate", () => {
@@ -193,7 +128,6 @@ myProgressBar.addEventListener("change", () => {
     (myProgressBar.value * player.duration) / 100;
 });
 
-// add to queue
 function addQueue() {
   if (!queue_music.find((item) => item.id == music_list[track_index].id))
     queue_music.push(
@@ -201,37 +135,49 @@ function addQueue() {
     );
 }
 
-// show queue
-function showQueue() {
-  var x = document.getElementById("qd");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+// backward 5 sec
+backwardBtn.addEventListener("click", () => {
+  player.currentTime -= 5;
+});
+function nextTrack() {
+  if (track_index < music_list.length - 1 && isRandom === false) {
+    track_index += 1;
+  } else if (track_index < music_list.length - 1 && isRandom === true) {
+    let random_index = Number.parseInt(Math.random() * music_list.length);
+    track_index = random_index;
   } else {
-    x.style.display = "none";
+    track_index = 0;
   }
-  const list = document.getElementById("qd");
-  list.innerHTML = "";
-  queue_music?.forEach((item, index) => {
-    const child = document.createElement("div");
-    const img = document.createElement("img");
-    const songname = document.createElement("h3");
-    const arname = document.createElement("h5");
-    songname.innerText = item.name;
-    arname.innerText = item.artistName;
-    child.appendChild(img);
-    child.appendChild(songname);
-    child.appendChild(arname);
-    child.style.padding = "0.5rem";
-    child.onclick = () => {
-      track_index = index;
-      now_playing.innerText = item.name;
-      audioPlay(item.previewURL);
-    };
-    list.appendChild(child);
-  });
+  // loadTrack(track_index);
+  now_playing.innerText = music_list[track_index].name;
+  audioPlay(music_list[track_index].previewURL);
 }
 
-// seaching song
+function prevTrack() {
+  if (track_index > 0) {
+    track_index -= 1;
+  } else {
+    track_index = music_list.length - 1;
+  }
+  now_playing.innerText = music_list[track_index].name;
+  audioPlay(music_list[track_index].previewURL);
+}
+next.onclick = nextTrack;
+previous.onclick = prevTrack;
+
+shuffle.onclick = () => {
+  isRandom = !isRandom;
+  shuffle.className = isRandom
+    ? "fa-solid fa-1x fa-shuffle shuffleon"
+    : "fa-solid fa-1x fa-shuffle";
+  let random_index = Number.parseInt(Math.random() * music_list.length);
+
+  if (isRandom) {
+    now_playing.innerText = music_list[random_index].name;
+    audioPlay(music_list[random_index].previewURL);
+  }
+};
+
 searchSongs.onkeydown = (event) => {
   const inputData = event.target.value.toLowerCase().split(" ").join("");
   const sd = document.getElementById("sd");
@@ -243,6 +189,7 @@ searchSongs.onkeydown = (event) => {
         .join("")
         .includes(event.target.value.toLowerCase().split(" ").join(""))
     );
+
     sd.innerHTML = "";
     searchedData?.forEach((item, index) => {
       const child = document.createElement("div");
@@ -260,25 +207,11 @@ searchSongs.onkeydown = (event) => {
         now_playing.innerText = item.name;
         audioPlay(item.previewURL);
       };
+      // const list = document.getElementById("music_list");
+      // an.appendChild(child);
       sd.appendChild(child);
     });
   } else {
     sd.innerHTML = "";
-  }
-};
-
-// add to playlist
-const addToPlaylist = () => {
-  const currentsong = music_list[track_index];
-  if (localStorage) {
-    let data = JSON.parse(localStorage.getItem("playlist"));
-    console.log(data, "data");
-    if (!data) {
-      data = [];
-    }
-    if (!data.find((item) => item.id == currentsong.id)) {
-      data.push(currentsong);
-      localStorage.setItem("playlist", JSON.stringify(data));
-    }
   }
 };
